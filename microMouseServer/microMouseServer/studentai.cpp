f0804281bac2a62357d2764acaa16019521bd5ae
RohitM2019 <rohitmittal@sfhs.com>
2018-06-24 01:39:50 -0700

    Started exploring mode

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -9,10 +9,12 @@ SHORTEST PATH PLAN
 #include "micromouseserver.h"
 #include "vector"
 #include "iostream"
+#include "string"
 
 //Enums! Enums galore!
 enum MouseState {FINDING_FINISH, BACKTRACKING, EXPLORING, COMPUTING_SHORTEST_PATH, END};
 enum MouseMovement {MOVE_FORWARD, TURN_LEFT, TURN_RIGHT, MOVE_BACKWARD};
+enum Direction {NORTH, EAST, SOUTH, WEST};
 
 //Forward declaration of Edge so Node can use it
 class Edge;
@@ -35,17 +37,19 @@ public:
     Node(int x_, int y_);
     int x;
     int y;
+    operator std::string() const;
     Edge * north = NULL;
     Edge * east = NULL;
     Edge * south = NULL;
     Edge * west = NULL;
+    std::vector<Edge*> edges;
 };
 
 class Edge
 {
 public:
     Edge();
-    Edge(Node * start_, Node * end_, int distance_);
+    Edge(Node * start_, int startDir, Node * end_, int endDir, int distance_);
     Node * start;
     Node * end;
     int distance;
@@ -61,8 +65,20 @@ void microMouseServer::studentAI()
     static int finishY = 0;
     static int direction = 0; //N = 0, E = 1, S = 2, W = 3
     static std::vector<MouseMovement> pastMoves; //Log of past movements (so I can go back to the start)
+    static bool firstRun = true;
 
-    //I'm so sorry
+    //Graph
+    static std::vector<Node> nodes;
+
+    //First run stuff. Add 0,0 to the graph and print that it's destination searching
+    if(firstRun)
+    {
+        printUI("Searching for finish...");
+        nodes.push_back(Node(0, 0));
+        firstRun = false;
+    }
+
+    //Helper functions...I'm so sorry
     static std::function<void(microMouseServer * server)> TurnLeft = [](microMouseServer * server)
     {
         server->turnLeft();
@@ -205,6 +221,83 @@ void microMouseServer::studentAI()
         }
         pastMoves.pop_back();
     }
+    else if(state == EXPLORING)
+    {
+        static bool isBacktracking = false;
+        static Node * lastVisitedNode = &nodes.at(0);
+        if(isBacktracking)
+        {
+
+        }
+        else
+        {
+            int pathCount = 0;
+            if(!isWallLeft())
+                pathCount++;
+            if(!isWallRight())
+                pathCount++;
+            if(!isWallForward())
+                pathCount++;
+
+            if (pathCount == 0)
+            {
+                TurnRight(this);
+                TurnRight(this);
+                MoveForward(this);
+                pastMoves.push_back(MOVE_BACKWARD);
+            }
+            else if (pathCount == 1)
+            {
+                //Default exploration routine
+                if(!isWallLeft())
+                {
+                    TurnLeft(this);
+                    MoveForward(this);
+                    pastMoves.push_back(TURN_LEFT);
+                    pastMoves.push_back(MOVE_FORWARD);
+                }
+                else if(!isWallForward())
+                {
+                    MoveForward(this);
+                    pastMoves.push_back(MOVE_FORWARD);
+                }
+                else
+                {
+                    TurnRight(this);
+                    MoveForward(this);
+                    pastMoves.push_back(TURN_RIGHT);
+                    pastMoves.push_back(MOVE_FORWARD);
+                }
+            }
+            else //Fork handler
+            {
+                Node * nodeAlreadyVisited = NULL;
+                for(Node node: nodes)
+                    if(node.x == x && node.y == y)
+                        nodeAlreadyVisited = &node;
+                if(nodeAlreadyVisited == NULL) //create a new node
+                {
+                    nodes.push_back(Node(x, y));
+
+                }
+                else //handler for reaching a node I already know about
+                {
+                    if(nodeAlreadyVisited->x != lastVisitedNode->x && nodeAlreadyVisited->y != lastVisitedNode->y) //if we just backtracked to the same node
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+    else if(state == COMPUTING_SHORTEST_PATH)
+    {
+        state = END;
+    }
+    else if(state == END)
+    {
+        foundFinish();
+    }
 }
 
 //MazeMap class definitions
@@ -219,7 +312,7 @@ int & MazeMap::operator()(int x, int y)
 {
     if(x < 0 || x > MAZE_WIDTH - 1 || y < 0 || y > MAZE_HEIGHT - 1) //if accessing out of bounds, return a reference to a junk integer
     {
-        int junk = INT_MAX;
+        int junk = INT_MAX; //memory leak risk, but it'll go out of scope as soon as StudentAI completes anyway.
         return junk;
     }
     return map[x][y];
@@ -238,6 +331,11 @@ Node::Node(int x_, int y_)
     y = y_;
 }
 
+Node::operator std::string() const
+{
+    return "(" + std::to_string(x) + "," + std::to_string(y) + ")";
+}
+
 Edge::Edge()
 {
     start = NULL;
@@ -245,9 +343,28 @@ Edge::Edge()
     distance = INT_MAX;
 }
 
-Edge::Edge(Node * start_, Node * end_, int distance_)
+Edge::Edge(Node * start_, int startDir, Node * end_, int endDir, int distance_)
 {
     start = start_;
     end = end_;
+
+    start->edges.push_back(this);
+    end->edges.push_back(this);
+
+    switch(startDir)
+    {
+    case 0: start->north = this; break;
+    case 1: start->east = this; break;
+    case 2: start->south = this; break;
+    case 3: start->west = this; break;
+    }
+    switch(endDir)
+    {
+    case 0: end->north = this; break;
+    case 1: end->east = this; break;
+    case 2: end->south = this; break;
+    case 3: end->west = this; break;
+    }
+
     distance = distance_;
 }
